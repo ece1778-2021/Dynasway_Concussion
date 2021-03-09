1c2c03564b73e32404cbf28ae356f3d78c29c2ec
Etsmsj Torres <etsmsj@gmail.com>
2021-03-09 02:35:27 -0500

    dynamic test ported to new code structure

--- app/src/main/java/com/example/dynaswayconcussion/Tests/DynamicTest/DynamicTest.java
+++ app/src/main/java/com/example/dynaswayconcussion/Tests/DynamicTest/DynamicTest.java
@@ -1,4 +1,158 @@
 package com.example.dynaswayconcussion.Tests.DynamicTest;
 
-public class DynamicTest {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.dynaswayconcussion.Tests.ITest;
+
+public class DynamicTest implements ITest, IStepCounterListener {
+
+    //Constants
+    private static final String INFO_TAG = "[Dynamic testing info]";
+    private static final long TEST_MIN_MILLIS = 20000; //the test is 20 seconds long
+    private static final long SENSOR_MIN_ACTIVATION_TIME = 10000; //the step sensor takes 10 seconds to activate to evade false positives
+    public static final int STEP_COUNTER_PERMISSION_CODE = 20;
+    private static final int TEST_FAILED = -1;
+
+    //Activity sensor related variables
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private boolean isSensorPresent = false;
+
+    //Initial values during test related variables
+    private int currentSteps = 0;
+    private boolean isDynamicTestReady = false;
+    private int stepsWhenTestStarted = 0;
+    private boolean testStarted = false;
+    private boolean sensorIsCounting = false;
+
+    //Test results related variables
+    private int stepsTakenDuringTest = 0;
+
+    //System related variables
+    Context ctx;
+    DoDynamicTestTask current_task = null;
+
+    //This class requires the following permissions for _ctx:
+    //Manifest.permission.ACTIVITY_RECOGNITION
+    //The sensor will fail if the given context doesn't have this permission
+    public DynamicTest(Context _ctx) {
+        this.ctx = _ctx;
+        mSensorManager = (SensorManager)this.ctx.getSystemService(Context.SENSOR_SERVICE);
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null)
+        {
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorPresent = true;
+        }
+        else
+        {
+            isSensorPresent = false;
+        }
+    }
+
+    @Override
+    public void startTest() {
+        if (isSensorPresent) {
+            stepsWhenTestStarted = currentSteps;
+            stepsTakenDuringTest = 0;
+            //startDynamicTestButton.setEnabled(false);
+            testStarted = true;
+            sensorIsCounting = false;
+            current_task = new DoDynamicTestTask();
+            current_task.execute();
+        }
+    }
+
+    @Override
+    public void stopTest() {
+        if (current_task != null) {
+            if (current_task.getStatus() == AsyncTask.Status.RUNNING
+                    || current_task.getStatus() == AsyncTask.Status.PENDING) {
+                current_task.cancel(true);
+                //To return -1 when the test has failed
+                stepsTakenDuringTest = TEST_FAILED;
+            }
+        }
+    }
+
+    @Override
+    public double getResult() {
+        return stepsTakenDuringTest;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        currentSteps = (int)event.values[0];
+        if (!isDynamicTestReady) {
+            isDynamicTestReady = true;
+            //startDynamicTestButton.setEnabled(true);
+        }
+        if (testStarted && !sensorIsCounting) {
+            sensorIsCounting = true;
+        }
+        Log.i(INFO_TAG, "Sensor changed, value: " + String.valueOf(currentSteps));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private class DoDynamicTestTask extends AsyncTask<Void, Void, Void> {
+
+
+        long startTime = 0;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //inAnimation = new AlphaAnimation(0f, 1f);
+            startTime = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            //startDynamicTestButton.setEnabled(true);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            timedStepsFunction(this.startTime);
+            return null;
+        }
+    }
+
+    private void timedStepsFunction(long startTime) {
+        //long currentTime = System.currentTimeMillis();
+        //long timeElapsed = currentTime - startTime;
+        /*while (timeElapsed < SENSOR_MIN_ACTIVATION_TIME) {
+            timeElapsed = System.currentTimeMillis() - startTime;
+            stepsWhenTestStarted = currentSteps;
+        }*/
+        while (testStarted && !sensorIsCounting) {
+            stepsWhenTestStarted = currentSteps;
+            //TODO: beep when this loop is done to tell the use the test started
+        }
+        startTime = System.currentTimeMillis();
+        long timeElapsed = System.currentTimeMillis() - startTime;
+        stepsWhenTestStarted = currentSteps;
+        while (timeElapsed < TEST_MIN_MILLIS) {
+            timeElapsed = System.currentTimeMillis() - startTime;
+            stepsTakenDuringTest = currentSteps - stepsWhenTestStarted;
+        }
+        float secondsElapsed = (timeElapsed / 1000.0f);
+        stepsTakenDuringTest = currentSteps - stepsWhenTestStarted;
+        String output = "Pace: 0 steps/s";
+        if (secondsElapsed > 0.0f) {
+            output = "Pace: " + String.valueOf(stepsTakenDuringTest / secondsElapsed) + " steps/s";
+        }
+        //steps_during_test_display.setText(output);
+
+        testStarted = false;
+        sensorIsCounting = false;
+    }
 }
--- app/src/main/java/com/example/dynaswayconcussion/Tests/DynamicTest/IStepCounterListener.java
+++ app/src/main/java/com/example/dynaswayconcussion/Tests/DynamicTest/IStepCounterListener.java
@@ -1,4 +1,9 @@
 package com.example.dynaswayconcussion.Tests.DynamicTest;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+
 public interface IStepCounterListener {
+    void onSensorChanged(SensorEvent event);
+    void onAccuracyChanged(Sensor sensor, int accuracy);
 }
