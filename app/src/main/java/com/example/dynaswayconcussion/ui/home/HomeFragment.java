3641dbba52cff09f1657927d8b626f7a1a0950dc
Etsmsj Torres <etsmsj@gmail.com>
2021-04-06 18:57:00 -0400

    notification on home page working, set concussion date working

--- app/src/main/java/com/example/dynaswayconcussion/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/dynaswayconcussion/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.dynaswayconcussion.ui.home;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,11 +13,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.dynaswayconcussion.R;
 import com.example.dynaswayconcussion.ui.CircularImageView;
+import com.example.dynaswayconcussion.ui.tests.StaticTestSelectionFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -27,13 +30,16 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HomeFragment extends Fragment {
@@ -54,6 +60,8 @@ public class HomeFragment extends Fragment {
     Button btnAboutApp;
 
     TextView hello_textview;
+    CardView notificationCard;
+    TextView notificationTextView;
 
     CircularImageView profile_imageview;
 
@@ -78,11 +86,13 @@ public class HomeFragment extends Fragment {
 
         hello_textview = (TextView)view.findViewById(R.id.home_hello_textview);
         profile_imageview = (CircularImageView)view.findViewById(R.id.home_profile_imageview);
+        notificationCard = (CardView)view.findViewById(R.id.notification_card);
+        notificationTextView = (TextView)view.findViewById(R.id.home_notification_textview);
 
         btnAboutConcussions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("http://concussionsontario.org/patienteducation/aboutconcussions-2/");
+                Uri uri = Uri.parse("http://concussionontario.org/patienteducation/aboutconcussions-2/");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
@@ -132,14 +142,16 @@ public class HomeFragment extends Fragment {
         btnLogConcussion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                uploadConcussionDate();
             }
         });
 
         btnAboutApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.nav_host_fragment, new AboutFragment()).addToBackStack( "backstack" );
+                fr.commit();
             }
         });
 
@@ -167,6 +179,7 @@ public class HomeFragment extends Fragment {
                 }
             }
         });
+        updateNotification();
     }
 
     @Override
@@ -236,6 +249,127 @@ public class HomeFragment extends Fragment {
             @Override
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(getActivity(), "Teams connection failed (Errno: 1)", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateNotification() {
+        final String REGULAR_STANDING_STRING = getResources().getString(R.string.static_test_regular);
+        final String TANDEM_STANDING_STRING = getResources().getString(R.string.static_test_tandem);
+        final String REGULAR_STANDING_COGNITIVE_STRING = getResources().getString(R.string.static_test_regular_dual_task);
+        final String TANDEM_STANDING_COGNITIVE_STRING = getResources().getString(R.string.static_test_tandem_dual_task);
+
+        final String REGULAR_DYNAMIC_STRING = getResources().getString(R.string.dynamic_test_regular);
+        final String TANDEM_DYNAMIC_STRING = getResources().getString(R.string.dynamic_test_tandem);
+        final String REGULAR_DYNAMIC_COGNITIVE_STRING = getResources().getString(R.string.dynamic_test_regular_dual_task);
+        final String TANDEM_DYNAMIC_COGNITIVE_STRING = getResources().getString(R.string.dynamic_test_tandem_dual_task);
+        String[] testTypeStrings = {REGULAR_STANDING_STRING, TANDEM_STANDING_STRING, REGULAR_STANDING_COGNITIVE_STRING, TANDEM_STANDING_COGNITIVE_STRING,
+                                    REGULAR_DYNAMIC_STRING, TANDEM_DYNAMIC_STRING, REGULAR_DYNAMIC_COGNITIVE_STRING, TANDEM_DYNAMIC_COGNITIVE_STRING};
+        Context toastContext = getActivity();
+        CollectionReference testsRef = db.collection("test_results");
+        Query notificationQuery = testsRef.whereEqualTo("is_baseline", true).whereEqualTo("user_uid", mAuth.getCurrentUser().getUid());
+        notificationQuery.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (queryDocumentSnapshots.size() > 0) {
+                    if (queryDocumentSnapshots.size() >= 8) {
+                        notificationCard.setCardBackgroundColor(getResources().getColor(R.color.main_green));
+                        notificationTextView.setText("No new notifications!");
+                    }
+                    else {
+                        int baselinesLeft = 8;
+                        List<Integer> existingBaselinesIDs = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            baselinesLeft--;
+                            existingBaselinesIDs.add(getTestTypeIDForNotification(document.getString("test_type")));
+                        }
+                        String resultNotification = "You have " + baselinesLeft + " baseline tests left to complete! Go ahead before the season starts! The following still haven't been completed: ";
+                        for (int i = 0; i < 8; i++) {
+                            boolean isInCompletedBaselines = false;
+                            for (Integer id : existingBaselinesIDs) {
+                                if (id.equals(i)) {
+                                    isInCompletedBaselines = true;
+                                    break;
+                                }
+                            }
+                            if (i < 8) {
+                                if (!isInCompletedBaselines) {
+                                    resultNotification += testTypeStrings[i] + ", ";
+                                }
+                            }
+                            else {
+                                if (!isInCompletedBaselines) {
+                                    resultNotification += testTypeStrings[i] + ".";
+                                }
+                            }
+                        }
+                        notificationTextView.setText(resultNotification);
+                    }
+                }
+                else {
+                    notificationTextView.setText("No baseline tests detected. You should do your baseline testing.");
+                }
+                notificationCard.setVisibility(View.VISIBLE);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(toastContext, "Failed to get test data", Toast.LENGTH_SHORT).show();
+                notificationCard.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private int getTestTypeIDForNotification(String testType) {
+        final String REGULAR_STANDING_STRING = getResources().getString(R.string.static_test_regular_constant);
+        final String TANDEM_STANDING_STRING = getResources().getString(R.string.static_test_tandem_constant);
+        final String REGULAR_STANDING_COGNITIVE_STRING = getResources().getString(R.string.static_test_regular_dual_task_constant);
+        final String TANDEM_STANDING_COGNITIVE_STRING = getResources().getString(R.string.static_test_tandem_dual_task_constant);
+
+        final String REGULAR_DYNAMIC_STRING = getResources().getString(R.string.dynamic_test_regular_constant);
+        final String TANDEM_DYNAMIC_STRING = getResources().getString(R.string.dynamic_test_tandem_constant);
+        final String REGULAR_DYNAMIC_COGNITIVE_STRING = getResources().getString(R.string.dynamic_test_regular_dual_task_constant);
+        final String TANDEM_DYNAMIC_COGNITIVE_STRING = getResources().getString(R.string.dynamic_test_tandem_dual_task_constant);
+
+        if (testType.equals(REGULAR_STANDING_STRING)) {
+            return 0;
+        }
+        else if (testType.equals(TANDEM_STANDING_STRING)) {
+            return 1;
+        }
+        else if (testType.equals(REGULAR_STANDING_COGNITIVE_STRING)) {
+            return 2;
+        }
+        else if (testType.equals(TANDEM_STANDING_COGNITIVE_STRING)) {
+            return 3;
+        }
+        else if (testType.equals(REGULAR_DYNAMIC_STRING)) {
+            return 4;
+        }
+        else if (testType.equals(TANDEM_DYNAMIC_STRING)) {
+            return 5;
+        }
+        else if (testType.equals(REGULAR_DYNAMIC_COGNITIVE_STRING)) {
+            return 6;
+        }
+        else if (testType.equals(TANDEM_DYNAMIC_COGNITIVE_STRING)) {
+            return 7;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    private void uploadConcussionDate() {
+        long timestamp = System.currentTimeMillis();
+        Map<String, Object> data = new HashMap<>();
+        data.put("timestamp", timestamp);
+        data.put("user_uid", mAuth.getCurrentUser().getUid());
+        Context toastContext = getActivity();
+        db.collection("injury_date").add(data).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                Toast.makeText(toastContext, "Concussion date set succesfully!", Toast.LENGTH_SHORT).show();
             }
         });
     }
