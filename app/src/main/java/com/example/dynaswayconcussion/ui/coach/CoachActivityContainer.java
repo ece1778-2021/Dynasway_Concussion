01ac6d8b2b1cb2caef56ae1b0bbde9b160da872b
Etsmsj Torres <etsmsj@gmail.com>
2021-04-06 16:24:01 -0400

    coach logout

--- app/src/main/java/com/example/dynaswayconcussion/ui/coach/CoachActivityContainer.java
+++ app/src/main/java/com/example/dynaswayconcussion/ui/coach/CoachActivityContainer.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.dynaswayconcussion.LoginActivity;
 import com.example.dynaswayconcussion.R;
 import com.example.dynaswayconcussion.ui.tests.StaticTestSelectionFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CoachActivityContainer extends AppCompatActivity {
 
@@ -21,5 +26,29 @@ public class CoachActivityContainer extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content,fragment, "tag")
                 .addToBackStack("backstack")
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.profile_activity_menu is a reference to an xml file named profile_activity_menu.xml
+        // which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.profile_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle menu button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //If log out button on action bar pressed, log out from Firebase and exit profile activity
+        if (id == R.id.logoutButton) {
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
