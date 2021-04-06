6857f983e32c5f88a8df6acc17f44eb1d5fa6c37
Etsmsj Torres <etsmsj@gmail.com>
2021-04-06 11:55:09 -0400

    QR code generator working

--- app/src/main/java/com/example/dynaswayconcussion/ui/home/TeamsConnectionFragment.java
+++ app/src/main/java/com/example/dynaswayconcussion/ui/home/TeamsConnectionFragment.java
@@ -1,5 +1,6 @@
 package com.example.dynaswayconcussion.ui.home;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,10 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dynaswayconcussion.R;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class TeamsConnectionFragment extends Fragment {
 
     String uid;
@@ -46,8 +51,17 @@ public class TeamsConnectionFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_teams_connection, container, false);
 
-        txtProfileUid = view.findViewById(R.id.teams_profile_id);
-        txtProfileUid.setText(uid);
+        //txtProfileUid = view.findViewById(R.id.teams_profile_id);
+        //txtProfileUid.setText(uid);
+
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(uid, BarcodeFormat.QR_CODE, 1000, 1000);
+            ImageView imageViewQrCode = (ImageView) view.findViewById(R.id.qr_code_imageview);
+            imageViewQrCode.setImageBitmap(bitmap);
+        } catch(Exception e) {
+
+        }
 
         return view;
     }
